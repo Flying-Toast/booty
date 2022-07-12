a0abecfecec4b8b9dfce5c3c8d7635667cdf5f9f
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-07-11 20:37:44 -0400

    vga scrolling

--- vga.c
+++ vga.c
@@ -8,16 +8,30 @@
 #define HEIGHT (25)
 
 static size_t cursorpos = 0;
+static uint8_t curr_clear_color = (VGA_COLOR_BLACK << 4) | VGA_COLOR_BLACK;
 static uint16_t upper_color_byte = (VGA_COLOR_BLACK << 4 | VGA_COLOR_WHITE) << 8;
 
 void vga_clear_screen(enum vga_color clear_color)
 {
-	memset((void *)VGABUF, clear_color | clear_color << 4, sizeof(*VGABUF) * WIDTH * HEIGHT);
+	curr_clear_color = (clear_color << 4) | clear_color;
+	memset((void *)VGABUF, curr_clear_color, sizeof(*VGABUF) * WIDTH * HEIGHT);
 	cursorpos = 0;
+}
+
+static void scroll_up(void)
+{
+	for (size_t i = 0; i < WIDTH * (HEIGHT - 1); ++i) {
+		VGABUF[i] = VGABUF[i + WIDTH];
+	}
+	cursorpos = WIDTH * (HEIGHT - 1);
+	memset((void *)(VGABUF + cursorpos), curr_clear_color, sizeof(*VGABUF) * WIDTH);
 }
 
 void vga_putc(char c)
 {
+	if (cursorpos >= WIDTH * HEIGHT)
+		scroll_up();
+
 	if (c == '\n') {
 		cursorpos += WIDTH;
 		cursorpos -= cursorpos % WIDTH;
