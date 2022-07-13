38f007fab444436535e9e250724e874a184edf16
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-07-13 11:14:48 -0400

    memcpy

--- kstd.c
+++ kstd.c
@@ -6,3 +6,10 @@ void *memset(void *s, uint8_t c, size_t n)
 		*((uint8_t *)s++) = c;
 	return s;
 }
+
+void *memcpy(void *dest, void *src, size_t n)
+{
+	while (n--)
+		((uint8_t *)dest)[n] = ((uint8_t *)src)[n];
+	return dest;
+}
--- kstd.h
+++ kstd.h
@@ -5,5 +5,6 @@
 #include <stdint.h>
 
 void *memset(void *s, uint8_t c, size_t n);
+void *memcpy(void *dest, void *src, size_t n);
 
 #endif
