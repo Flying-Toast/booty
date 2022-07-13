434ebb53622a7d839c95cf0e2e278e7a16d0714e
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-07-13 11:54:16 -0400

    vga buffering

--- vga.c
+++ vga.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include "vga.h"
+#include "kstd.h"
 #include "panic.h"
 
 #define VGABUF ((volatile uint8_t *) 0xA0000)
@@ -14,13 +15,14 @@
 			panic(); \
 	} while (0)
 
+static uint8_t drawbuf[SCREEN_WIDTH * SCREEN_HEIGHT - 1];
 static uint8_t drawcolor = 0;
 
 void vga_clear_screen(uint8_t color)
 {
 	const uint32_t colorword = color | color<<8 | color<<16 | color<<24;
-	for (size_t i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(*VGABUF)/sizeof(colorword); ++i)
-		((volatile uint32_t *)VGABUF)[i] = colorword;
+	for (size_t i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(*drawbuf)/sizeof(colorword); ++i)
+		((volatile uint32_t *)drawbuf)[i] = colorword;
 }
 
 void vga_set_draw_color(uint8_t color)
@@ -36,7 +38,12 @@ void vga_fill_rect(unsigned x, unsigned y, unsigned width, unsigned height)
 
 	for (unsigned cy = y; cy < y + height; ++cy) {
 		for (unsigned cx = x; cx < x + width; ++cx) {
-			VGABUF[cy*SCREEN_WIDTH + cx] = drawcolor;
+			drawbuf[cy*SCREEN_WIDTH + cx] = drawcolor;
 		}
 	}
+}
+
+void vga_present(void)
+{
+	memcpy(VGABUF, drawbuf, sizeof(drawbuf));
 }
